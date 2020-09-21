3e28782dd94c1d95601fa945914b47837db82e72
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2020-09-21 12:24:02 +0200

    Add license headers

--- src/main/kotlin/eu/openanalytics/shinyproxyoperator/components/PodTemplateSpecFactory.kt
+++ src/main/kotlin/eu/openanalytics/shinyproxyoperator/components/PodTemplateSpecFactory.kt
@@ -1,13 +1,28 @@
+/**
+ * ShinyProxy-Operator
+ *
+ * Copyright (C) 2020 Open Analytics
+ *
+ * ===========================================================================
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License as published by
+ * The Apache Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License for more details.
+ *
+ * You should have received a copy of the Apache License
+ * along with this program.  If not, see <http://www.apache.org/licenses/>
+ */
 package eu.openanalytics.shinyproxyoperator.components
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import eu.openanalytics.shinyproxyoperator.crd.ShinyProxy
 import eu.openanalytics.shinyproxyoperator.crd.ShinyProxyInstance
 import io.fabric8.kubernetes.api.model.*
-import io.fabric8.kubernetes.client.internal.SerializationUtils
-import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature
-import io.fabric8.kubernetes.client.utils.Serialization
 
 class PodTemplateSpecFactory {
 
