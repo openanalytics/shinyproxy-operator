092690ec141c8b284daf4609c245c09a8a77177b
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2024-02-15 17:10:25 +0100

    Fix #32380: update license headers

--- src/main/kotlin/eu/openanalytics/shinyproxyoperator/controller/ResourceRetriever.kt
+++ src/main/kotlin/eu/openanalytics/shinyproxyoperator/controller/ResourceRetriever.kt
@@ -1,7 +1,7 @@
 /**
  * ShinyProxy-Operator
  *
- * Copyright (C) 2021-2023 Open Analytics
+ * Copyright (C) 2021-2024 Open Analytics
  *
  * ===========================================================================
  *
